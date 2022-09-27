ed54e7f778a1a121805e92552fd625c25785fccc
Nicolás Olivera <nnicolivera@hotmail.com>
2022-09-27 01:17:24 -0300

    agrego modal

---
+++ main.js
@@ -0,0 +1,9 @@
+function modal() {
+    Swal.fire({
+        title: '¡Recuerda!',
+        text: 'Es temporada de haters, mantente fresco :)',
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 13500
+    });
+}
